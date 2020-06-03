721904d8159977eb5b38391a7f8ca8d9fc1cdd3f
Muhammad Haseeb <58140479+haseebpyd@users.noreply.github.com>
2020-06-03 11:28:10 +0500

    Add files via upload

---
+++ Advanced Web Scraping using Scrapy/quotes_spider/spiders/quotes.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = (
+        'http://quotes.toscrape.com/',
+    )
+
+    def parse(self, response):
+        quotes = response.xpath('//*[@class="quote"]')
+        for quote in quotes:
+            text = quote.xpath('.//*[@class="text"]/text()').extract_first()
+            author = quote.xpath('.//*[@itemprop="author"]/text()').extract_first()
+            tags = quote.xpath('.//*[@itemprop="keywords"]/@content').extract_first()
+
+            yield{'Text': text,
+                  'Author': author,
+                  'Tags': tags}
+
+        next_page_url = response.xpath('//*[@class="next"]/a/@href').extract_first()
+        absolute_next_page_url = response.urljoin(next_page_url)
+        yield scrapy.Request(absolute_next_page_url)
