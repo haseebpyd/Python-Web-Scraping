3698fd8815feafad320038499167f7f8a5765496
Muhammad Haseeb <58140479+haseebpyd@users.noreply.github.com>
2020-06-03 11:26:09 +0500

    Add files via upload

---
+++ Basic Web Scraping with Scrapy/quotes_spider/spiders/quotes.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = (
+        'http://quotes.toscrape.com/',
+    )
+
+    def parse(self, response):
+        h1_tag = response.xpath('//h1/a/text()').extract_first()
+        tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+
+        yield {'H1 Tag': h1_tag, 'Tags': tags}
